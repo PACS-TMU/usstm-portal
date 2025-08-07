f764c08b0ab3c1655690f7bf34b9528cb2216f93
grcng <diep.nguyen@torontomu.ca>
2025-08-07 15:09:32 -0400

    Added initial file structure with Next.JS 15 and Supabase

---
+++ README.md
@@ -0,0 +1,3 @@
+# Undergraduate Science Society (USSTM) Portal
+
+This is a portal for all student groups under USSTM to log in and manage their events, finances, and more!
---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const defaultUrl = process.env.NEXT_PUBLIC_BASE_URL
+    ? `https://${process.env.NEXT_PUBLIC_BASE_URL}`
+    : "http://localhost:3000";
+
+console.log("Base: ", defaultUrl);
+
+export const metadata: Metadata = {
+    metadataBase: new URL(defaultUrl),
+    title: "USSTM Portal",
+    description:
+        "The Undergraduate Science Society (USSTM) Portal - You can find all the important links and details here for your USSTM student groups.",
+};
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    display: "swap",
+    subsets: ["latin"],
+});
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${geistSans.className} antialiased`}>
+                {children}
+            </body>
+        </html>
+    );
+}
